f5116c1b552160bf69f82c7e2c59807f3c25580d
Jagmohan-cmd <141887538+Jagmohan-cmd@users.noreply.github.com>
2023-10-18 16:28:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
 //your code here
+let arr = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
+function getArticleLess (input){
+    let words = input.split("");
+    let nonArticleWords = words.filter(
+        (word) => {
+            let lowerCase = word.toLowerCase();
+            if(lowerCase === "the" || lowerCase === "an" || lowerCase === "a"){
+                return false;
+            }
+            return true;
+        }
+    )
+    return nonArticleWords.join(" ");
+}
+let hashMap = {};
+let arr1 = [];
+
+for(i = 0; i < arr.length; i++){
+    let articleLess = nonArticleWords(arr[i]);
+    hashMap[articleLess] = arr[i];
+    arr1.push(articleLess);
+}
+
+arr1.sort();
+
+let output = [];
+
+arr1.forEach((newString) => {
+    output.push(hashMap[newString]);
+})
+
+console.log(output);
